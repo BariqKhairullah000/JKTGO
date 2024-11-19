0e5e8f9fe82b222323c729734a10787b12c607cd
BariqKhairullah000 <ariq.syas@gmailcom>
2024-11-19 12:54:34 +0700

    Membuat Halaman Profile

--- app/src/main/java/com/bangkitbariq/jktgo/ProfileActivity.kt
+++ app/src/main/java/com/bangkitbariq/jktgo/ProfileActivity.kt
@@ -3,37 +3,38 @@ package com.bangkitbariq.jktgo
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkitbariq.jktgo.MainActivity
 import com.bangkitbariq.jktgo.chatbot.ChatbotActivity
-import com.bangkitbariq.jktgo.ProfileActivity
+import com.bangkitbariq.jktgo.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
 
         // FloatingActionButton untuk Chatbot
         val fabChatbot: FloatingActionButton = findViewById(R.id.fab_chatbot)
         fabChatbot.setOnClickListener {
-            val intent = Intent(this@MainActivity, ChatbotActivity::class.java)
+            val intent = Intent(this, ChatbotActivity::class.java)
             startActivity(intent)
         }
 
         // BottomNavigationView untuk navigasi
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigation)
-        bottomNavigationView.selectedItemId = R.id.item_dashboard // Tandai item aktif
+        bottomNavigationView.selectedItemId = R.id.item_profile // Tandai item aktif
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.item_profile -> {
-                    val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+                R.id.item_dashboard -> {
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
-                    finish() // Tutup MainActivity agar tidak ada banyak aktivitas
+                    finish() // Tutup ProfileActivity
                     true
                 }
-                R.id.item_dashboard -> true // Tetap di halaman ini
+                R.id.item_profile -> true // Tetap di halaman ini
                 else -> false
             }
         }
